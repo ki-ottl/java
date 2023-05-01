afc526c16f8ce50ebb2b5ee4a55f2cbd57436f0c
JIWON <kiottl@naver.com>
2023-05-01 15:21:25 +0900

    .

--- 자바프로그래밍/0425/team project/Character3.java
+++ 자바프로그래밍/0425/team project/Character3.java
@@ -2,66 +2,145 @@ import java.util.Random;
 
 public class Character3 extends Base {
 
-	Character3(String name) { // 아르마딜로
-      this.name = name;
-      this.hp = 180;
-      this.power = 30;
-   }
+	Character3(String name) {
+		this.name = name;
+		this.maxHp = 80;
+		this.hp = 80;
+		this.power = 10;
+	} // 아르마딜로 : 동글동글 귀엽지만 튼튼한 등딱지를 가진 아르마딜로 (방어 캐릭터 / 방어가 가능해서 공격받은 양에서 10만큼의 데미지가 감소됩니다.)
 
 	@Override
 	public void underAttack(int attack) {
 		Random r = new Random();
 		int inputNum = r.nextInt(3) + 1;
 
-		int attackResult = attack;
-
-		if (inputNum == 1) {
-
-			System.out.println();
-			System.out.println("┌─────────┐");
-			System.out.println("│　　　　　│");
-			System.out.println("└∩───∩────┘");
-			System.out.println("(`･ω･´)");
-			System.out.println();
-			System.out.println(this.name + "(이)가 방패를 사용하여 공격을 회피했다!");
-			attackResult = attack - 10;
-			
-		} else if (inputNum == 2) {
-			System.out.println();
-			System.out.println("╭◜◝ ͡ ◜◝ ╮");
-			System.out.println("(Ꮚ´꒳`Ꮚ");
-			System.out.println("└╰ ͜ ◟◞╯");
-			System.out.println();
-			System.out.println(this.name + "(이)가 둥글게 몸을 말아 공격을 회피했다!");
-			attackResult = attack - 20;
-		} else if (inputNum == 3) {
-			System.out.println();
-			System.out.println(".　∧∧");
-			System.out.println(" ( *'ω'*)");
-			System.out.println("  (　　)");
-			System.out.println("　ｖｖ");
-			System.out.println();
-			System.out.println("　  川　　폴-짝");
-			System.out.println("  ((　))");
-			System.out.println();
-			System.out.println(this.name + "(이)가 폴짝 뛰어 공격을 회피했다!");
-			attackResult = attack - 30;
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 
-      System.out.println(this.name+"(이)가 "+attack+"만큼 공격을 받았지만, 방어에 성공하여 "+attackResult+"만큼 데미지를 받았다" );
-      this.hp = this.hp - attackResult;
-   }
+		if (this.hp - attack < 0) { // 남은 hp에 비해 큰 공격을 받은 경우
+			System.out.println(this.name + "가 " + "-" + attack + "만큼 데미지를 받고 죽었다.");
+			this.hp = this.hp - attack;
+		} else {
+
+			if (inputNum == 1) {
+
+				System.out.println();
+				System.out.println("┌─────────┐");
+				System.out.println("│　　　　　│");
+				System.out.println("└∩───∩────┘");
+				System.out.println("(`･ω･´)");
+				System.out.println();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(this.name + "(이)가 방패를 사용하여 공격을 회피했다!");
+				System.out.println();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			} else if (inputNum == 2) {
+				System.out.println();
+				System.out.println("╭◜◝ ͡ ◜◝ ╮");
+				System.out.println("(Ꮚ´꒳`Ꮚ");
+				System.out.println("└╰ ͜ ◟◞╯");
+				System.out.println();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(this.name + "(이)가 둥글게 몸을 말아 공격을 회피했다!");
+				System.out.println();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			} else if (inputNum == 3) {
+				System.out.println();
+				System.out.println(".　∧∧");
+				System.out.println(" ( *'ω'*)");
+				System.out.println("  (　　)");
+				System.out.println("　ｖｖ");
+				System.out.println();
+				System.out.println("　  川　　폴-짝");
+				System.out.println("  ((　))");
+				System.out.println();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(this.name + "(이)가 폴짝 뛰어 공격을 회피했다!");
+				System.out.println();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			int attackResult = attack - 10; // 방어에 성공하여 10만큼 데미지를 감소한다
+
+			if (attackResult == 0) { // 0인 경우
+				System.out.println(this.name + "가 " + "방어에 성공하여 데미지를 받지 않았다!");
+			} else {
+				System.out.println(this.name + "가 " + "방어에 성공하여 " + "-" + attackResult + "만큼 데미지를 받았다!");
+				this.hp = this.hp - attackResult; // 방어 후 데미지만큼 hp 감소
+			}
+
+		}
+
+		System.out.println();
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 	@Override
 	public int Attack() {
 		Random r = new Random();
 		int inputNum = r.nextInt(3) + 1;
 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(this.name + "가 공격을 시작합니다!");
+		System.out.println();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		if (inputNum == 1) {
 
 			this.power = 10;
 			System.out.println();
 			System.out.println(this.name + "(이)가 '굴러가며 박치기'를 시전했다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println();
 			System.out.println("　　　빙");
 			System.out.println("　　　글");
@@ -71,13 +150,28 @@ public class Character3 extends Base {
 			System.out.println("　　　　　　빙");
 			System.out.println("　　　　　　글");
 			System.out.println();
-			System.out.println(this.name + "(이)가 '굴러가며 박치기'로 " + this.power + " 데미지로 공격했습니다.");
-
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(this.name + "(이)가 '굴러가며 박치기'로 " + this.power + " 데미지로 공격했습니다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		} else if (inputNum == 2) {
-         this.power = 20;
+			this.power = 20;
 			System.out.println();
 			System.out.println(this.name + "(이)가 '꼬리 휘두르기'를 시전했다!");
 			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println("　　 　 ∧＿∧");
 			System.out.println("　　　　(´∀｀)");
 			System.out.println("　　 　/　　　ヽ");
@@ -86,11 +180,27 @@ public class Character3 extends Base {
 			System.out.println("　　　　  /　人　(");
 			System.out.println("　　　    (＿ノ (＿_)");
 			System.out.println();
-			System.out.println(this.name + "(이)가 '꼬리 휘두르기'로 " + this.power + " 데미지로 공격했습니다.");
-      } else if (inputNum == 3) {
-         this.power = 30;
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(this.name + "(이)가 '꼬리 휘두르기'로 " + this.power + " 데미지로 공격했습니다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} else if (inputNum == 3) {
+			this.power = 30;
 			System.out.println();
 			System.out.println(this.name + "(이)가 '돌주먹 날리기'를 시전했다!");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println();
 			System.out.println("∧＿∧");
 			System.out.println("(　･ω･)＝つ≡つ");
@@ -98,9 +208,16 @@ public class Character3 extends Base {
 			System.out.println("./　　　) 바바바바");
 			System.out.println("( /￣∪");
 			System.out.println();
-			System.out.println(this.name + "(이)가 '돌주먹 날리기'로 " + this.power + " 데미지로 공격했습니다.");
-      }
-    
-      return power;
-   }
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(this.name + "(이)가 '돌주먹 날리기'로 " + this.power + " 데미지로 공격했습니다!");
+			System.out.println();
+		}
+
+		return power;
+	}
 }
+
--- 자바프로그래밍/0425/team project/Character5.java
+++ 자바프로그래밍/0425/team project/Character5.java
@@ -1,69 +1,172 @@
-
 import java.util.Random;
 
 public class Character5 extends Base {
 
 	Character5(String name) {
 		this.name = name;
-		this.hp = 100;
+		this.maxHp = 70;
+		this.hp = 70;
 		this.power = 10;
-	} // 쿠키 : 먹는 음식에 따라 성질이 변하는 쿠키
+	} // 쿠키 : 다중인격 쿠키 ( 공격 캐릭터 / 먹는 음식에 따라서 다른 쿠키가 되어 공격 필살기가 달라집니다.)
 
 	@Override
 	public int Attack() {
 		Random r = new Random();
 		int inputNum = r.nextInt(5) + 1;
 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(this.name + "가 공격을 시작합니다!");
+		System.out.println();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		if (inputNum == 1) {
-			
+
 			this.power = 10;
 			System.out.println();
 			System.out.println(this.name + "(이)가 독버섯을 먹고 독버섯맛 쿠키가 되었다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println(this.name + "(이)가 '독버섯을 던져 독구름 발생시키기'를 시전했다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
 			System.out.println();
 			System.out.println("(⊃｡•́‿•̀｡)⊃━✿✿✿✿✿✿");
 			System.out.println();
-			System.out.println(this.name + "(이)가 " + this.power + " 데미지로 공격했습니다.");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println(this.name + "(이)가 " + this.power + " 데미지로 공격했습니다!");
+			System.out.println();
 
 		} else if (inputNum == 2) {
-			
-			this.power = 20;
+
+			this.power = 15;
 			System.out.println();
 			System.out.println(this.name + "(이)가 프로틴을 먹고 근육맛 쿠키가 되었다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println(this.name + "(이)가 '항아리처럼 생긴 케틀벨 던지기'를 시전했다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
 			System.out.println();
 			System.out.println("(ノ ˘_˘)ノ　ζ|||ζ　ζ|||ζ　ζ|||ζ");
 			System.out.println();
-			System.out.println(this.name + "(이)가 " + this.power + " 데미지로 공격했습니다.");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println(this.name + "(이)가 " + this.power + " 데미지로 공격했습니다!");
+			System.out.println();
 
 		} else if (inputNum == 3) {
-			
-			this.power = 30;
+
+			this.power = 20;
 			System.out.println();
 			System.out.println(this.name + "(이)가 당근을 먹고 당근맛 쿠키가 되었다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println(this.name + "(이)가 '단단한 당근을 연속으로 뽑아 던지기'를 시전했다!");
 			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println("(っ’-‘)╮ =͟͟͞͞=͟͟͞͞ṑṑṑ=͟͟͞͞=͟͟͞͞ṑṑṑ=͟͟͞͞=͟͟͞͞ṑṑṑ");
 			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println(this.name + "(이)가 " + this.power + " 데미지로 공격했습니다.");
+			System.out.println();
 
 		} else if (inputNum == 4) {
-			
-			this.power = 40;
+
+			this.power = 25;
 			System.out.println();
 			System.out.println(this.name + "(이)가 자색고구마를 먹고 자색 고구마맛 쿠키가 되었다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
 			System.out.println(this.name + "(이)가 '고구마 곤봉을 연타하여 때리기'를 시전했다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println();
 			System.out.println("(＃￣□￣)o━∈・・━━━━☆");
 			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println();
 			System.out.println(this.name + "(이)가 " + this.power + " 데미지로 공격했습니다.");
+			System.out.println();
 
 		} else if (inputNum == 5) {
-			
-			this.power = 50;
+
+			this.power = 30;
 			System.out.println();
 			System.out.println(this.name + "(이)가 캡사이신을 먹고 캡사이신맛 쿠키가 되었다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println(this.name + "(이)가 '캡사이신 마그마 폭발'을 시전했다!");
+			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println();
 			System.out.println("...Λ＿Λ");
 			System.out.println("（ㆍωㆍ)つ━☆*。");
@@ -71,8 +174,14 @@ public class Character5 extends Base {
 			System.out.println("　し-Ｊ　　　°。로 *´¨)");
 			System.out.println("　　　　　　..　.· ´¸.·롱*´¨) ¸.·*¨)");
 			System.out.println("　　　　　　　　　　(¸.·´ (.·¸.'*");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println();
-			System.out.println(this.name + "(이)가 " + this.power + " 데미지로 공격했습니다.");
+			System.out.println(this.name + "(이)가 " + this.power + " 데미지로 공격했습니다!");
+			System.out.println();
 
 		}
 		return power;
@@ -87,11 +196,17 @@ public class Character5 extends Base {
 			System.out.println();
 			System.out.println("   뽀  각  !");
 			System.out.println("╰( (ᗒᗩᗕ) )━");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println();
 			System.out.println(this.name + "(이)가 공격을 받아 몸에 금이 갔다!");
+			System.out.println();
 
 		} else if (inputNum == 2) {
-			
+
 			System.out.println();
 			System.out.println("╭ ◜◝ ͡ ◜◝ ͡ ◜◝ ͡ ◜◝ ͡ ◜◝ ͡ ◜◝ ͡ ◜◝ ͡ ◜◝ ͡ ◜◝╮");
 			System.out.println("                 충     격                  ");
@@ -101,19 +216,37 @@ public class Character5 extends Base {
 			System.out.println("/}__/}");
 			System.out.println("( • ▼•)");
 			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println(this.name + "(이)가 공격에 큰 충격을 받았다!");
+			System.out.println();
 
 		} else if (inputNum == 3) {
-			
+
 			System.out.println();
 			System.out.println("✧*｡ヾ(｡>﹏<｡)ﾉﾞ✧*。");
 			System.out.println();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			System.out.println(this.name + "(이)가 공격에 땀을 흘려 눅눅해졌다!");
-			
-		}
+			System.out.println();
 
-		System.out.println(this.name + "(이)가 " + attack + "만큼 데미지를 받았습니다.");
+		}
 		System.out.println();
+		System.out.println(this.name + "가 " + "-" + attack + " 데미지를 받았습니다!");
+		System.out.println();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		this.hp = this.hp - attack;
 
 	}
 }
