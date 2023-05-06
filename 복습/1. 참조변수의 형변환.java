974856254ebe27fe5a798852d907bdbef84f3623
JIWON <122678325+ki-ottl@users.noreply.github.com>
2023-05-06 11:36:26 +0900

    Update 1. 참조변수의 형변환.java

--- 복습/1. 참조변수의 형변환.java
+++ 복습/1. 참조변수의 형변환.java
@@ -7,9 +7,9 @@ public class Ex7_7 { // 참조변수의 형변환
 		FireEngine fe2 = null;
 
 		fe.water();
-		car = fe; // car = (Car)fe;에서 형변환이 생략 가능.
+		car = fe; // car = (Car)fe;에서 형변환 생략 가능.
 		// car.water(); // 컴파일 에러. Car타입의 참조변수로는 water() 호출 불가!
-		fe = (FireEngine) car; // 부모타입을 자손타입에 대입. 형변환 생략 불가.
+		fe = (FireEngine) car; // 조상타입을 자손타입에 대입. 형변환 생략 불가.
 		fe.water();
 
 	}
