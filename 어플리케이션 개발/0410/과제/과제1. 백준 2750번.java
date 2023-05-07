fc20579c48b48e4eae70d95ee5e7e9c8184ea10c
JIWON <122678325+ki-ottl@users.noreply.github.com>
2023-05-07 13:18:45 +0900

    Update 과제1. 백준 2750번.java

--- 어플리케이션 개발/0410/과제/과제1. 백준 2750번.java
+++ 어플리케이션 개발/0410/과제/과제1. 백준 2750번.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Quiz1 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
 //		첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 
 //		둘째 줄부터 N개의 줄에는 수가 주어진다. 
@@ -25,42 +26,28 @@ public class Quiz1 {
 //		3
 //		4
 //		5
-
-		int N;
-		N = sc.nextInt();
-
-		int[] arr = new int[N];
-
-		for (int i = 0; i < N; i++) {
+		
+		int num = sc.nextInt();
+		
+		int[] arr = new int[num];
+		
+		for (int i = 0 ; i < arr.length ; i++) {
 			arr[i] = sc.nextInt();
 		}
-
-		int tmp;
-
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] == arr[j]) {
-					arr[i] = -1;
-				}
-			}
-		}
-
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] > arr[j]) {
-					tmp = arr[i];
+		
+		for (int i = 0 ; i < arr.length ; i++) {
+			for (int j = i+1 ; j<arr.length ; j++) {
+				if (arr[i]>arr[j]) {
+					int tmp = arr[i];
 					arr[i] = arr[j];
 					arr[j] = tmp;
 				}
 			}
 		}
-
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] != -1)
-				System.out.println(arr[i]);
+		
+		for (int i = 0 ; i < arr.length ; i++) {
+			System.out.println(arr[i]);
 		}
-
-		System.out.println();
-
 	}
 }
+
