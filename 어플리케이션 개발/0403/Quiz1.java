d5997c1d0af3bedb961f0425edf1e79e1a6695bf
JIWON <kiottl@naver.com>
2023-05-07 11:12:33 +0900

    Update Quiz1.java

--- 어플리케이션 개발/0403/Quiz1.java
+++ 어플리케이션 개발/0403/Quiz1.java
@@ -1,57 +1,49 @@
-import java.util.Arrays;
-
-public class Quiz1 {
-
+public class Test {
+	
 	public static void main(String[] args) {
-		
-		// 총점을 구하여 높은 등수부터 출력 
+		// 총점을 구하여 높은 등수부터 출력
 		// 1등 : E(270)
 		// 2등 : D(255)
 		// 3등 : C(240)
 		// 4등 : B(210)
 		// 5등 : A(155)
-		
-		String[] name  = {"A", "B", "C", "D", "E"};
-		
-		int[] kor = {50, 60, 70, 80, 90};
-		int[] eng = {10, 70, 80, 90, 100};
-		int[] math = {95, 80, 90, 85, 80};
+
+		String[] name = { "A", "B", "C", "D", "E" };
+
+		int[] kor = { 50, 60, 70, 80, 90 };
+		int[] eng = { 10, 70, 80, 90, 100 };
+		int[] math = { 95, 80, 90, 85, 80 };
 		
 		int[] total = new int[name.length];
 		
-		for(int i=0; i<name.length;i++) {
+		for(int i = 0 ; i < total.length ; i++) {
 			total[i] = kor[i]+eng[i]+math[i];
 		}
 		
-		int[] sort = new int[name.length];
+		int[] sort = new int[total.length];
 		
-		for(int i=0; i<name.length;i++) {
+		for(int i = 0 ; i < sort.length ; i++) {
 			sort[i] = total[i];
 		}
 		
-		Arrays.sort(sort);
 		
-		int tmp;
-		String tmpForName;
-		
-		for(int i = 0 ; i<sort.length ; i++) {
-			for (int j = i+1; j < sort.length; j++) {
+		for (int i = 0 ; i < sort.length ; i++) {
+			for (int j = i+1 ; j < sort.length ; j++) {
 				if (sort[i]<sort[j]) {
-					tmp = sort[i];
+					int tmp = sort[i];
 					sort[i] = sort[j];
 					sort[j] = tmp;
-					tmpForName = name[i];
+					String tmpName = name[i];
 					name[i] = name[j];
-					name[j] = tmpForName;
+					name[j] = tmpName;
 				}
 			}
 		}
 		
-		
-		for (int i = 0 ; i < name.length ; i++) {
+		for (int i = 0 ; i < name.length; i++) {
 			System.out.println((i+1)+"등 : "+name[i]+"("+sort[i]+")");
 		}
-
+		
 	}
 
 }
